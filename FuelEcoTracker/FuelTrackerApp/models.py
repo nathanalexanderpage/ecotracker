c747d68b57b581d4628c5b5bbbb0d51486c35641
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-08-17 20:00:28 -0700

    models successfully created

--- FuelEcoTracker/FuelTrackerApp/models.py
+++ FuelEcoTracker/FuelTrackerApp/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
+from datetime import date
 
 # Create your models here.
-class Receipt(models.Model):
-    vehicle = models.ForeignKey(Vehicle)
-    gas_station = models.ForeignKey('gas station', FuelStation)
-    date = models.DateField(default=date.today)
-    time = models.TimeField(default=timezone.now)
-    gallons = models.FloatField()
-    price_per_gal = models.FloatField('price per gallon')
-    is_cash_purchase = models.BooleanField('cash purchase')
-
 class Vehicle(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     make = models.CharField(max_length=40)
     model = models.CharField(max_length=40)
     year = models.IntegerField()
@@ -23,3 +16,12 @@ class FuelStation(models.Model):
     address = models.CharField(max_length=100)
     latitude = models.FloatField(blank=True)
     longitude = models.FloatField(blank=True)
+
+class Receipt(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    gas_station = models.ForeignKey(FuelStation, on_delete=models.CASCADE)
+    receipt_date = models.DateField(default=date.today)
+    receipt_time = models.TimeField(default=timezone.now)
+    gallons = models.FloatField()
+    price_per_gal = models.FloatField()
+    is_cash_purchase = models.BooleanField()
