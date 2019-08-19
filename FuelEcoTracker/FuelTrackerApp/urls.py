9a268e131fd9cf902762462be568b3f6d8af37e3
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-08-19 01:23:06 -0700

    added urls and views for index/new/detail pages for Receipts, Vehicles, Gas Stations; views.py doesn't yet render the templates.

--- FuelEcoTracker/FuelTrackerApp/urls.py
+++ FuelEcoTracker/FuelTrackerApp/urls.py
@@ -3,5 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /
     path('', views.index, name='index'),
+    # ex: /station/new/
+    path('station/new/', views.fuel_station_new, name='fuel_station_new'),
+    # ex: /station/5/
+    path('station/<int:fuel_station_id>/', views.fuel_station_detail, name='fuel_station_detail'),
+    # ex: /purchase/new/
+    path('purchase/new/', views.receipt_new, name='receipt_new'),
+    # ex: /purchase/3/
+    path('purchase/<int:receipt_id>/', views.receipt_detail, name='receipt_detail'),
+    # ex: /vehicle/new/
+    path('vehicle/new/', views.vehicle_new, name='vehicle_new'),
+    # ex: /vehicle/4/
+    path('vehicle/<int:vehicle_id>/', views.vehicle_detail, name='vehicle_detail'),
 ]
--- FuelEcoTracker/FuelTrackerApp/views.py
+++ FuelEcoTracker/FuelTrackerApp/views.py
@@ -1,7 +1,27 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from .models import FuelStation, Receipt, Vehicle
 
 # Create your views here.
-from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world. This is Fuel Eco Tracker")
+
+def fuel_station_detail(request, fuel_station_id):
+    return HttpResponse("You're looking at gas station %s" % fuel_station_id)
+
+def fuel_station_new(request):
+    return HttpResponse("You're making a new gas station")
+
+def receipt_detail(request, receipt_id):
+    return HttpResponse("You're looking at receipt %s" % receipt_id)
+
+def receipt_new(request):
+    return HttpResponse("You're inputting a new receipt")
+
+def vehicle_detail(request, vehicle_id):
+    return HttpResponse("You're looking at vehicle %s" % vehicle_id)
+
+def vehicle_new(request):
+    return HttpResponse("You're making a new vehicle")
