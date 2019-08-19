8950c5ff8d9fb736dd49c40960bc93210c7c3623
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-08-18 17:01:00 -0700

    migrations complete for Vehicle, Receipt, FuelStation

--- FuelEcoTracker/FuelTrackerApp/admin.py
+++ FuelEcoTracker/FuelTrackerApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Vehicle, Receipt, FuelStation
 
 # Register your models here.
+admin.site.register(Vehicle)
+admin.site.register(Receipt)
+admin.site.register(FuelStation)
--- FuelEcoTracker/FuelTrackerApp/models.py
+++ FuelEcoTracker/FuelTrackerApp/models.py
@@ -1,7 +1,7 @@
+from datetime import date, datetime
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-from datetime import date
 
 # Create your models here.
 class Vehicle(models.Model):
@@ -9,19 +9,42 @@ class Vehicle(models.Model):
     make = models.CharField(max_length=40)
     model = models.CharField(max_length=40)
     year = models.IntegerField()
-    nickname = models.CharField(max_length=40)
+    nickname = models.CharField(null=True, blank=True, max_length=40)
+
+    def __str__(self):
+        return ((self.nickname + ', ') if self.nickname else '') + ' ' + self.owner.username + '\'s ' + str(self.year) + ' ' + self.make + ' ' + self.model
 
 class FuelStation(models.Model):
     company = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-    latitude = models.FloatField(blank=True)
-    longitude = models.FloatField(blank=True)
+    address = models.CharField(max_length=80)
+    city = models.CharField(max_length=50)
+    state_abbr = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.city + ', ' + self.state_abbr + ' ' + self.company + ' @ ' + self.address + ' (' + self.zip_code + ')'
 
 class Receipt(models.Model):
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     gas_station = models.ForeignKey(FuelStation, on_delete=models.CASCADE)
-    receipt_date = models.DateField(default=date.today)
-    receipt_time = models.TimeField(default=timezone.now)
+    receipt_datetime = models.DateTimeField(default=datetime.now)
+    # receipt_date = models.DateField(default=date.today)
+    # receipt_time = models.TimeField(default=timezone.now)
     gallons = models.FloatField()
     price_per_gal = models.FloatField()
     is_cash_purchase = models.BooleanField()
+
+    def __str__(self):
+        if self.vehicle.nickname:
+            vehicle_name = self.vehicle.nickname + ' (' + self.vehicle.make + ' ' + self.vehicle.model + ')'
+        else:
+            vehicle_name = self.vehicle.make + ' ' + self.vehicle.model
+        return self.vehicle.owner.username + '\'s ' + vehicle_name + ': ' + str(self.receipt_datetime)[:16] + ' @ ' + self.gas_station.company + ', ' + str(self.gallons) + 'gal w/ ' + ('cash' if self.is_cash_purchase else 'card') + ' — $' + str(round(self.gallons * self.price_per_gal, 2))
+
+    def is_very_expensive(self):
+        return price_per_gal > 3.9
+
+    def is_very_cheap(self):
+        return price_per_gal < 3.1
