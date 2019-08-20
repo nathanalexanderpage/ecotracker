06fc2cb5e40b7ee4211135e7afa4ad8d9dcd7472
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-08-20 00:27:17 -0700

    added index page urls, views, templates for each branch: Receipt, Vehicle, FuelStation; prototype semantic nav bar on receipt detail page; created new vehicle, new fuel station forms

--- FuelEcoTracker/FuelTrackerApp/urls.py
+++ FuelEcoTracker/FuelTrackerApp/urls.py
@@ -14,20 +14,26 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
 
     # STATION PATTERN
-    # ex: /station/new/
-    path('station/new/', views.fuel_station_new, name='fuel_station_new'),
-    # ex: /station/5/
-    path('station/<int:fuel_station_id>/', views.fuel_station_detail, name='fuel_station_detail'),
+    # ex: /stations/
+    path('stations/', views.fuel_station_index, name='receipt_index'),
+    # ex: /stations/new/
+    path('stations/new/', views.fuel_station_new, name='fuel_station_new'),
+    # ex: /stations/5/
+    path('stations/<int:fuel_station_id>/', views.fuel_station_detail, name='fuel_station_detail'),
 
     # PURCHASE PATTERN
-    # ex: /purchase/new/
-    path('purchase/new/', views.receipt_new, name='receipt_new'),
-    # ex: /purchase/3/
-    path('purchase/<int:receipt_id>/', views.receipt_detail, name='receipt_detail'),
+    # ex: /purchases/
+    path('purchases/', views.receipt_index, name='receipt_index'),
+    # ex: /purchases/new/
+    path('purchases/new/', views.receipt_new, name='receipt_new'),
+    # ex: /purchases/3/
+    path('purchases/<int:receipt_id>/', views.receipt_detail, name='receipt_detail'),
 
     # VEHICLE PATTERN
-    # ex: /vehicle/new/
-    path('vehicle/new/', views.vehicle_new, name='vehicle_new'),
-    # ex: /vehicle/4/
-    path('vehicle/<int:vehicle_id>/', views.vehicle_detail, name='vehicle_detail'),
+    # ex: /vehicles/
+    path('purchases/', views.receipt_index, name='vehicle_index'),
+    # ex: /vehicles/new/
+    path('vehicles/new/', views.vehicle_new, name='vehicle_new'),
+    # ex: /vehicles/4/
+    path('vehicles/<int:vehicle_id>/', views.vehicle_detail, name='vehicle_detail'),
 ]
--- FuelEcoTracker/FuelTrackerApp/views.py
+++ FuelEcoTracker/FuelTrackerApp/views.py
@@ -6,28 +6,19 @@ from .models import FuelStation, Receipt, Vehicle
 
 # Create your views here.
 
-app_url_ext = '/fuel'
-
 # INDEX VIEWS
 def index(request):
-    context = {
-        'subsite_base_url': app_url_ext
-    }
-    return render(request, 'FuelTrackerApp/index.html', context)
+    return render(request, 'FuelTrackerApp/index.html')
 
 # AUTH VIEWS
 def login(request):
-    context = {
-        'subsite_base_url': app_url_ext
-    }
-    return render(request, 'FuelTrackerApp/auth/login.html', context)
+    return render(request, 'FuelTrackerApp/auth/login.html')
 def signup(request):
-    context = {
-        'subsite_base_url': app_url_ext
-    }
-    return render(request, 'FuelTrackerApp/auth/signup.html', context)
+    return render(request, 'FuelTrackerApp/auth/signup.html')
 
 # FUEL STATION VIEWS
+def fuel_station_index(request):
+    return render(request, 'FuelTrackerApp/fuel_station/index.html')
 def fuel_station_detail(request, fuel_station_id):
     try:
         fuel_station = FuelStation.objects.get(pk=fuel_station_id)
@@ -41,9 +32,10 @@ def fuel_station_new(request):
     return HttpResponse("You're making a new gas station")
 
 # RECEIPT VIEWS
+def receipt_index(request):
+    return render(request, 'FuelTrackerApp/receipt/index.html')
 def receipt_detail(request, receipt_id):
     try:
-        print(0)
         print('Searching for Receipt object with pk=' + str(receipt_id))
         receipt = Receipt.objects.get(pk=receipt_id)
         print(receipt)
@@ -51,8 +43,6 @@ def receipt_detail(request, receipt_id):
         fuel_station = FuelStation.objects.get(pk=receipt.gas_station_id)
         print('Searching for Vehicle object with pk=' + str(receipt.vehicle_id))
         vehicle = Vehicle.objects.get(pk=receipt.vehicle_id)
-        # print('Searching for User object with pk=' + str(vehicle.owner_id))
-        # owner = settings.AUTH_USER_MODEL.objects.get(pk=receipt.owner_id)
     except:
         raise Http404("Indicated purchase does not exist")
     context = {
@@ -67,14 +57,26 @@ def receipt_new(request):
     return HttpResponse("You're inputting a new receipt")
 
 # VEHICLE VIEWS
+def vehicle_index(request):
+    return render(request, 'FuelTrackerApp/vehicle/index.html')
 def vehicle_detail(request, vehicle_id):
     try:
         vehicle = Vehicle.objects.get(pk=vehicle_id)
+        receipts =  Receipt.objects.filter(vehicle_id=vehicle.id)
+        print(receipts)
     except:
-        raise Http404("Indicated fuel station does not exist")
+        raise Http404("Indicated vehicle does not exist")
     context = {
-        'vehicle': vehicle
+        'vehicle': vehicle,
+        'receipts': receipts
     }
     return render(request, 'FuelTrackerApp/vehicle/detail.html', context)
 def vehicle_new(request):
-    return HttpResponse("You're making a new vehicle")
+    try:
+        vehicles = Vehicle.objects.all()
+    except:
+        vehicles = False
+    context = {
+        'vehicles': vehicles
+    }
+    return render(request, 'FuelTrackerApp/vehicle/new.html', context)
