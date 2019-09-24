2c06f440eaee0b38cc159a62855f0db0194b9074
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-09-23 20:16:45 -0700

    changed URL path not to require '/fuel/'

--- FuelEcoTracker/FuelEcoTracker/urls.py
+++ FuelEcoTracker/FuelEcoTracker/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('fuel/', include('FuelTrackerApp.urls')),
+    path('', include('FuelTrackerApp.urls')),
     path('admin/', admin.site.urls),
 ]
