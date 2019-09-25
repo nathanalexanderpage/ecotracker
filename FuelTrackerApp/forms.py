bdef4a531ebb1b6b54f5299d85545b7e465ec4ef
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-09-25 04:53:03 -0700

    removed extraneous folder from root level holding entire repo contents

---
+++ FuelTrackerApp/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import FuelStation, Receipt, Vehicle
+
+class FuelStationForm(ModelForm):
+	class Meta:
+		model = FuelStation
+		fields = ['id', 'company', 'address', 'city', 'state_abbr', 'zip_code']
+
+class ReceiptForm(ModelForm):
+	class Meta:
+		model = Receipt
+		fields = ['id', 'gallons', 'price_per_gal', 'is_cash_purchase', 'gas_station', 'vehicle', 'receipt_datetime', 'note', 'mi_since_last']
+
+class UserForm(ModelForm):
+	class Meta:
+		model = User
+		fields = ['username', 'password', 'email']
+
+class VehicleForm(ModelForm):
+	class Meta:
+		model = Vehicle
+		fields = ['id', 'make', 'model', 'year', 'nickname', 'owner']
---
+++ FuelTrackerApp/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import seializers
+from .models import FuelStation, Receipt, Vehicle
+
+# configure serializers here
+class FuelStationSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = FuelStation
+		fields = ('id', 'company', 'address', 'latitude', 'longitude', 'city', 'state_abbr', 'zip_code')
+class ReceiptSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Receipt
+		fields = ('id', 'gallons', 'price_per_gal', 'is_cash_purchase', 'gas_station_id', 'vehicle_id', 'receipt_datetime', 'note', 'mi_since_last')
+class VehicleSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Vehicle
+		fields = ('id', 'make', 'model', 'year', 'nickname', 'owner_id')
